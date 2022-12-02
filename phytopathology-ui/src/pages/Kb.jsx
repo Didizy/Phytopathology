152864809a5b123646bb9718aa7d99f40f7d41cc
Artemy Nikishin <am_nikishin@mail.ru>
2022-12-02 13:53:31 +1000

    888

--- phytopathology-ui/src/pages/Kb.jsx
+++ phytopathology-ui/src/pages/Kb.jsx
@@ -33,6 +33,9 @@ function Kb() {
       {/* <TableBase /> */}
 //dev commit
 //feature commit
+//new feature commit
+//hotfix
+//new feature commit update
       <Box component={Paper} sx={{ height: 700, textAlign: "center" }}>
         <TabContext value={value}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
